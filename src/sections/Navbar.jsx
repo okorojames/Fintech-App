93ca7294f046e370c797450724b53a3bf80a2bec
Okoro James <okorojameschizaram@gmail.com>
2023-05-11 21:37:24 +0100

    almost done

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -6,12 +6,37 @@ import { useRef } from "react";
 const Navbar = () => {
   //
   const navLinks = useRef();
-  const hamburger = useRef();
+  const burger__one = useRef();
+  const burger__two = useRef();
+  const burger__three = useRef();
+  const nav__overlay = useRef();
   //
   const handleToggleNav = () => {
     navLinks.current.classList.toggle("toggleNav");
-    console.log(Array.of(hamburger)[0].current.children);
+    burger__one.current.classList.toggle("toggleNav");
+    burger__two.current.classList.toggle("toggleNav");
+    burger__three.current.classList.toggle("toggleNav");
+    nav__overlay.current.classList.toggle("toggleNav");
   };
+  //
+  const handleCloseNav = () => {
+    navLinks.current.classList.remove("toggleNav");
+    burger__one.current.classList.remove("toggleNav");
+    burger__two.current.classList.remove("toggleNav");
+    burger__three.current.classList.remove("toggleNav");
+    nav__overlay.current.classList.remove("toggleNav");
+  };
+  //
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 1012) {
+      navLinks.current.classList.remove("toggleNav");
+      burger__one.current.classList.remove("toggleNav");
+      burger__two.current.classList.remove("toggleNav");
+      burger__three.current.classList.remove("toggleNav");
+      nav__overlay.current.classList.remove("toggleNav");
+    }
+    // console.log(window.innerWidth);
+  });
   //
   return (
     <header>
@@ -39,11 +64,16 @@ const Navbar = () => {
             Download
           </a>
         </div>
-        <div className="hamburger" onClick={handleToggleNav} ref={hamburger}>
-          <div className="burger burger__one"></div>
-          <div className="burger burger__two"></div>
-          <div className="burger burger__three"></div>
+        <div className="hamburger" onClick={handleToggleNav}>
+          <div className="burger burger__one" ref={burger__one}></div>
+          <div className="burger burger__two" ref={burger__two}></div>
+          <div className="burger burger__three" ref={burger__three}></div>
         </div>
+        <div
+          className="nav__overlay"
+          onClick={handleCloseNav}
+          ref={nav__overlay}
+        ></div>
       </nav>
     </header>
   );
